114d0de27cc7a83a05efbbb9b324cabb3d38422c
yanyuchuang <751523479@qq.com>
2013-09-04 14:05:19 +0800

    commit

---
+++ search/src/com/joyplus/data/SearchThread.java
@@ -0,0 +1,21 @@
+package com.joyplus.data;
+
+public class SearchThread extends Thread{
+	
+	public SearchThread(){
+		
+	}
+	
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		try{
+			
+		}catch(Exception e){
+			e.printStackTrace();
+		}finally{
+			
+		}
+	}
+	
+}
--- search/src/com/joyplus/search/MainActivity.java
+++ search/src/com/joyplus/search/MainActivity.java
@@ -11,4 +11,29 @@ public class MainActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 	}
+
+	@Override
+	protected void onResume() {
+		// TODO Auto-generated method stub
+		super.onResume();
+	}
+
+	@Override
+	protected void onPause() {
+		// TODO Auto-generated method stub
+		super.onPause();
+	}
+
+	@Override
+	protected void onStop() {
+		// TODO Auto-generated method stub
+		super.onStop();
+	}
+
+	@Override
+	protected void onDestroy() {
+		// TODO Auto-generated method stub
+		super.onDestroy();
+	}
+	
 }
